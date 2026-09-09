c05be8a2c2f57a955b2bd5eb60e45ea8541ad176
Ritika Chopra <r.chopra@vyos.io>
2026-09-09 16:08:36 -0700

    T9238: radius: validate RADIUS Message-Authenticator in Access responses when present

--- accel-pppd/radius/radius_p.h
+++ accel-pppd/radius/radius_p.h
@@ -4,12 +4,15 @@
 #include <netinet/in.h>
 #include <pthread.h>
 #include <stdarg.h>
+#include <stddef.h>
 
 #include "triton.h"
 #include "radius.h"
 #include "ppp.h"
 #include "ipdb.h"
 #include "pwdb.h"
+
+#define HMAC_MD5_LEN 16
 
 struct rad_server_t;
 
@@ -217,6 +220,7 @@ int rad_req_send(struct rad_req_t *req);
 int __rad_req_send(struct rad_req_t *req, int async);
 int rad_req_read(struct triton_md_handler_t *h);
 int verify_response_authenticator(struct rad_req_t *req, struct rad_packet_t *pack);
+int verify_message_authenticator(struct rad_req_t *req, struct rad_packet_t *pack);
 
 struct radius_pd_t *find_pd(struct ap_session *ses);
 int rad_proc_attrs(struct rad_req_t *req);
@@ -240,6 +244,7 @@ int rad_packet_recv(int fd, struct rad_packet_t **, struct sockaddr_in *addr);
 void rad_packet_free(struct rad_packet_t *);
 void rad_packet_print(struct rad_packet_t *pack, struct rad_server_t *s, void (*print)(const char *fmt, ...));
 int rad_packet_send(struct rad_packet_t *pck, int fd, struct sockaddr_in *addr);
+int hmac_md5(const uint8_t *key, size_t key_len, const uint8_t *data, size_t data_len, uint8_t out[HMAC_MD5_LEN]);
 
 void dm_coa_cancel(struct radius_pd_t *pd);
 
